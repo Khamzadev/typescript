c35928999fcd56f5a510c73e3a387d245c6e6e87
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2023-01-03 15:49:06 +0600

    init : typed props and functions

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Card from './components/Card';
+import Card, { CardVariant } from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <Card width="200px" height="200px">
+      <Card
+        onClick={(num) => console.log('click', num)}
+        variant={CardVariant.primary}
+        width="200px"
+        height="200px">
         <button>Кнопка</button>
       </Card>
     </div>
--- react-typescript/src/components/Card.tsx
+++ react-typescript/src/components/Card.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, PropsWithChildren, useState } from 'react';
 
-export enum CardVariant{
+export enum CardVariant {
   outlined = 'outlined',
   primary = 'primary',
 }
@@ -8,12 +8,25 @@ export enum CardVariant{
 interface CardProps {
   width?: string;
   height?: string;
-  children?: React.ReactChild | React.ReactNode;
-  variant:CardVariant;
+  variant: CardVariant;
+  onClick: (num: number) => void;
 }
 
-const Card: FC<CardProps> = ({ width, height, children }) => {
-  return <div style={{ width, height, border: '2px solid gray' }}>{children}</div>;
+const Card: FC<PropsWithChildren<CardProps>> = ({ width, height, children, variant, onClick }) => {
+  const [state, setState] = useState(0);
+
+  return (
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+        background: variant === CardVariant.primary ? 'lightgray' : '',
+      }}
+      onClick={() => onClick(state)}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
