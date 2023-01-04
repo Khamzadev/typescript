addb6a5e99690a43a4d1580e1ceaf521dbb56b49
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2023-01-04 14:22:39 +0600

    init: create new componetns and add types file

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 function App() {
+  const users: IUser[] = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+      },
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      address: {
+        street: 'Victor Plains',
+        city: 'Wisokyburgh',
+        zipcode: '90566-7771',
+      },
+    },
+  ];
   return (
     <div className="App">
       <Card
@@ -11,6 +35,7 @@ function App() {
         height="200px">
         <button>Кнопка</button>
       </Card>
+      <UserList users={users} />
     </div>
   );
 }
